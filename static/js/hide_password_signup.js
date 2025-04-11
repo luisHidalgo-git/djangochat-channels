dbf218854149bbd9b02a5a574e58468e731d6983
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2025-04-11 13:38:13 -0600

    hide password

---
+++ static/js/hide_password_signup.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.toggle-password').forEach(button => {
+    button.addEventListener('click', function () {
+        const passwordInput = this.closest('.input-group').querySelector('input');
+        const icon = this.querySelector('i');
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            icon.classList.remove('fa-eye');
+            icon.classList.add('fa-eye-slash');
+        } else {
+            passwordInput.type = 'password';
+            icon.classList.remove('fa-eye-slash');
+            icon.classList.add('fa-eye');
+        }
+    });
+});
